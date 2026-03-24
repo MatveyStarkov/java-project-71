cf459c28c5bddcd58647379a95ee2458c01d87bc
MattStarkov <starkovmatthew@yandex.ru>
2026-03-25 03:01:14 +0500

    init commit of gradle project

---
+++ app/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("com.github.ben-manes.versions") version "0.53.0"
+    application
+    id("java")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
