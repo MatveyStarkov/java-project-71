50b26b00ac664c3b3d4bc9e8337b6ca42e51fa36
MattStarkov <starkovmatthew@yandex.ru>
2026-03-26 14:35:12 +0500

    add makefile, add picocli (-h, -v)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("info.picocli:picocli:4.7.6")
 }
 
 tasks.test {
